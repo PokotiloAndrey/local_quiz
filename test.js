de2c96335d078fdc3244f340b7a85205df08e6ad
Kemplix <113040903+PokotiloAndrey@users.noreply.github.com>
2025-05-20 03:19:24 +0300

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@
  * @jest-environment jsdom
  */
 import { describe,expect, jest } from '@jest/globals';
-import Quiz from './script.js';
+const Quiz = require('./script.js');
 
 delete window.location;
 window.location = { href: "" };
