ab0a353ae1842dec7526a4b70f98cc066aca188b
Kemplix <113040903+PokotiloAndrey@users.noreply.github.com>
2025-05-20 03:48:21 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
 export default {
   testEnvironment: 'jsdom',
   transform: {},
-  extensionsToTreatAsEsm: ['.js'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1'
-  },
-  transformIgnorePatterns: []
+  }
 };
