fdc74cd835eda4a12b784a8a62dd7a3725873696
Kemplix <113040903+PokotiloAndrey@users.noreply.github.com>
2025-05-20 03:37:30 +0300

    Update jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,10 @@
 module.exports = {
   testEnvironment: 'jsdom',
   transform: {},
+  globals: {
+    'window': {}
+  },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1'
-  },
-  transformIgnorePatterns: []
+  }
 };
